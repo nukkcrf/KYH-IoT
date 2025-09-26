7a248b50bbeeda36034411c191ce5c478e25550b
Gabriel Uifalean <gabriel13ro@gmail.com>
2025-09-26 13:38:17 +0200

    Add project files.

---
+++ KYH-IoT/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace KYH_IoT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var thingspeak = new ThingSpeak();
+                int temperature = new Random().Next(0, 40);
+                int humidity = new Random().Next(0, 100);
+                int rpm = new Random().Next(1800, 7000);
+                int speedKmH = new Random().Next(0, 110);
+                int fuelLevel = new Random().Next(0, 100);
+                Console.WriteLine($"Temperature: {temperature} °C, Humidity: {humidity} %, RPM: {rpm}, Speed: {speedKmH} km/h, Fuel Level: {fuelLevel} %");
+
+                thingspeak.SendData(temperature, humidity, rpm, speedKmH, fuelLevel);
+                System.Threading.Thread.Sleep(15000);
+
+            }
+
+        }
+    }
+}
---
+++ KYH-IoT/ThingSpeak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KYH_IoT
+{
+    internal class ThingSpeak
+    {
+        internal void SendData(int temperature, int humidity, int fuelLevel, int speedKmH, int rpm )
+        {
+            // GET https://api.thingspeak.com/update?api_key=D7HQNNWLRGS38LCN&field1=0
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://api.thingspeak.com");
+            var response = httpClient.GetAsync($"/update?api_key=D7HQNNWLRGS38LCN&field1={temperature}&field2={humidity}&field3={fuelLevel}&field4={speedKmH}&field5={rpm}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Data sent to ThingSpeak successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to send data to ThingSpeak. Status code: {response.StatusCode}");
+            }
+
+           // throw new NotImplementedException();
+        }
+    }
+}
